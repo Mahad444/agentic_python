cec015569a4c2d76e79c8fa5e991e3e31c01461b
Ally Juma Said <mahadsaid95@icloud.com>
2025-05-13 10:49:49 -0700

    learning agentic AI using Python

---
+++ first.py
@@ -0,0 +1,14 @@
+print ("Hello world")
+
+# This is a string
+
+name = "Mahad Ali"
+age = 20
+height = 5.9
+#  boolean
+is_student = True
+
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Is Student?",is_student)
