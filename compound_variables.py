a7ddf2f61a823a392dad7896fe3c82f3c29ef118
Ally Juma Said <mahadsaid95@icloud.com>
2025-05-28 21:28:46 -1100

    Add examples for accessing elements in a tuple

--- compound_variables.py
+++ compound_variables.py
@@ -9,3 +9,8 @@ my_frozenset = frozenset([10, 20, 30, 40, 50])
 # Accessing elements in a list
 first_element = my_list[0]  # 1
 second_element = my_list[1]  # 2
+third_element = my_list[2]  # 3
+# Accessing elements in a tuple
+first_tuple_element = my_tuple[0]  # 10
+second_tuple_element = my_tuple[1]  # 20
+third_tuple_element = my_tuple[2]  # 30
