43beaef7cc22c0cc9b7b0902abf71b61bdb62a37
Ally Juma Said <mahadsaid95@icloud.com>
2025-07-04 23:06:30 -0700

    Add replay functionality to the number guessing game

--- project_mini.py
+++ project_mini.py
@@ -22,3 +22,28 @@ if attempts == 0:
     print("Sorry, you've run out of attempts. The number was", secret_number)
      
 
+    # Create another instance of the game
+    play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+    if play_again == "yes":
+        # You can wrap the game logic in a function and call it again
+        def play_game():
+            secret_number = random.randint(1, 15)
+            attempts = 3
+            print("I am thinking of a number between 1 and 15.")
+            while attempts > 0:
+                guess = int(input("Take a guess: "))
+                if guess == secret_number:
+                    print("Congratulations! You guessed the number.")
+                    return
+                elif guess < secret_number:
+                    print("Your guess is too low.")
+                elif guess > secret_number:
+                    print("Your guess is too high.")
+                attempts -= 1
+                if attempts > 0:
+                    print("Wrong guess. You have", attempts, "attempts left.")
+            print("Sorry, you've run out of attempts. The number was", secret_number)
+        play_game()
+    else:
+        print("Thanks for playing!")
+
